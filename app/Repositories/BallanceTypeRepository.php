8c2ca287be276ebc412f5de614810c2d2d16c154
Denis <drodman@ukr.net>
2019-10-18 12:04:39 +0300

    Add Spenndings Repository Interface

--- app/Repositories/BallanceTypeRepository.php
+++ app/Repositories/BallanceTypeRepository.php
@@ -1,13 +1,12 @@
 <?php
 namespace App\Repositories;
 
-use App\Account;
 use App\Repositories\Interfaces\BallanceTypeInterface;
 
 class BallanceTypeRepository implements BallanceTypeInterface
 {
-    public function getBallanceTypesArrByAccount(Account $account)
+    public function getBallanceTypesArrByAccount($account)
     {
-        return BallanceType::where('account_id',$this->acc_id)->get()->toArray();
+        return BallanceType::where('account_id',$account->id)->get()->toArray();
     }
 }
--- app/Repositories/Interfaces/BallanceTypeInterface.php
+++ app/Repositories/Interfaces/BallanceTypeInterface.php
@@ -3,9 +3,8 @@
 namespace App\Repositories\Interfaces;
 
 
-use App\Account;
 
 interface BallanceTypeInterface
 {
-    public function getBallanceTypesArrByAccount(Account $account);
+    public function getBallanceTypesArrByAccount($account);
 }
